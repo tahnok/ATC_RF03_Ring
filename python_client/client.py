62d154fd37bcd5985c06a65573aa968141694c8b
Wesley Ellis <tahnok@gmail.com>
2024-08-02 22:21:25 -0300

    refactor to use queues to communicate between request and response

--- python_client/client.py
+++ python_client/client.py
@@ -1,5 +1,12 @@
 """
 A python client for connecting to the Colmi R02 Smart ring
+
+
+TODO:
+    - fetch "steps"
+    - get "Stress"
+    - make nicer methods for getting data from callback
+    - "scan" mode instead of hard coded address
 """
 
 import asyncio
@@ -22,6 +29,7 @@ CMD_START_HEART_RATE = 105 # 0x69
 CMD_STOP_HEART_RATE = 106 # 0x6A
 CMD_BATTERY = 3
 CMD_BLINK_TWICE = 16 # 0x10
+CMD_GET_STEP_SOMEDAY = 67 #0x43
 
 def make_packet(command_key: int, sub_data: bytearray|None = None) -> bytearray:
     packet = bytearray(16)
@@ -47,41 +55,83 @@ START_SPO2_PACKET = make_packet(CMD_START_HEART_RATE, bytearray(b'\x03\x25'))
 STOP_SPO2_PACKET = make_packet(CMD_STOP_HEART_RATE, bytearray(b'\x03\x00\x00'))
 
 BATTERY_PACKET = make_packet(CMD_BATTERY)
+
 BLINK_TWICE_PACKET = make_packet(CMD_BLINK_TWICE)
 
-FLAG_ERROR_MASK = 127
+GET_TODAY_STEPS_PACKET = make_packet(CMD_GET_STEP_SOMEDAY, bytearray(b'\x00\x0F\x00\x5F\x01'))
 
-def parse_response(packet: bytearray) -> dict[str, int]:
-    if packet[0] > 127: # high bit is set
-        assert False, f"Packet has error bit set {packet}"
+def parse_sport_detail_packet(packet: bytearray):
+    assert len(packet) == 16
+    assert packet[0] == CMD_GET_STEP_SOMEDAY
+    assert packet[1] != 255, "Packet request malformed"
+    offset = 0
+    new_calorie_protocol = False
 
-    packet_type = packet[0]
-    if packet_type == CMD_START_HEART_RATE:
-        return {
-                "type": packet[1],
-                "errCode": packet[2],
-                "value": packet[3],
-                }
-    elif packet_type == CMD_STOP_HEART_RATE:
-        return {}
-    elif packet_type == CMD_BATTERY:
-        return {
-                "batteryLevel": packet[1],
-                "charging": bool(packet[2]),
-                }
+    if packet[1] == 240:
+        if packet[3] == 1:
+            new_calorie_protocol = True
+        offset = 1
+        #
     else:
-        assert False, f"unknown packet type {packet}"
+        #year
+        ...
 
-async def get_heart_rate(client, rx_char) -> None:
-    await client.write_gatt_char(rx_char, START_HEART_RATE_PACKET, response=False)
+async def send_packet(client: BleakClient, rx_char, packet: bytearray) -> None:
+    await client.write_gatt_char(rx_char, packet, response=False)
+
+
+def parse_heart_rate(packet: bytearray) -> dict[str, int]:
+    return {
+            "type": packet[1],
+            "error_code": packet[2],
+            "value": packet[3],
+            }
+
+def empty_parse(packet: bytearray) -> None:
+    """Used for commands that we expect a response, but there's nothing in the response"""
+    return None
+
+def parse_battery(packet: bytearray) -> dict[str, int]:
+    r"""
+    example: bytearray(b'\x03@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00C')
+    """
+    return {
+            "battery_level": packet[1],
+            "charging": bool(packet[2]),
+            }
+
+def log_packet(packet: bytearray) -> None:
+    print("received: ", packet)
+
+# these are commands that we expect to have a response returned for
+COMMAND_HANDLERS = {
+        CMD_BATTERY: parse_battery,
+        CMD_START_HEART_RATE: parse_heart_rate,
+        CMD_STOP_HEART_RATE: empty_parse,
+        CMD_GET_STEP_SOMEDAY: log_packet,
+    }
+
+async def get_heart_rate(client, rx_char, queues: dict[int, asyncio.Queue]) -> None:
+    await send_packet(client, rx_char, START_HEART_RATE_PACKET)
     print("wrote HR reading packet, waiting...")
 
-    for _ in range(16):
-        await asyncio.sleep(2)
-        await client.write_gatt_char(rx_char, CONTINUE_HEART_RATE_PACKET, response=False)
-        print(".")
+    valid_hr = []
+    tries = 0
+    while len(valid_hr) < 6 and tries < 20:
+        try:
+            data = await asyncio.wait_for(queues[CMD_START_HEART_RATE].get(), 2)
+            if data["error_code"] == 1:
+                print("No heart rate detected, probably not on")
+                break
+            if data["value"] != 0:
+                valid_hr.append(data["value"])
+        except TimeoutError:
+            print(".")
+            tries += 1
+            await client.write_gatt_char(rx_char, CONTINUE_HEART_RATE_PACKET, response=False)
 
     await client.write_gatt_char(rx_char, STOP_HEART_RATE_PACKET, response=False)
+    print(valid_hr)
 
 
 async def get_spo2(client, rx_char) -> None:
@@ -94,47 +144,60 @@ async def get_spo2(client, rx_char) -> None:
 
     await client.write_gatt_char(rx_char, STOP_SPO2_PACKET, response=False)
 
+async def get_battery(client, rx_char, queues: dict[int, asyncio.Queue]):
+    await send_packet(client, rx_char, BATTERY_PACKET)
+    return await queues[CMD_BATTERY].get()
+
+
+async def get_device_info(client: BleakClient):
+    data = {}
+    device_info_service = client.services.get_service(DEVICE_INFO_UUID)
+    assert device_info_service
+
+    hw_info_char = device_info_service.get_characteristic(DEVICE_HW_UUID)
+    assert hw_info_char
+    hw_version = await client.read_gatt_char(hw_info_char)
+    data["hw_version"] = hw_version.decode("utf-8")
+
+    fw_info_char = device_info_service.get_characteristic(DEVICE_FW_UUID)
+    assert fw_info_char
+    fw_version = await client.read_gatt_char(fw_info_char)
+    data["fw_version"] = fw_version.decode("utf-8")
+
+    return data
+
+
 async def main():
     print("Connecting...")
 
-    def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
-        print("received:", data)
-        print(parse_response(data))
+    queues = { cmd: asyncio.Queue() for cmd in COMMAND_HANDLERS.keys() }
+
+    def handle_rx(_: BleakGATTCharacteristic, packet: bytearray):
+        packet_type = packet[0]
+        assert packet_type < 127, f"Packet has error bit set {packet}"
+
+        if packet_type in COMMAND_HANDLERS:
+            queues[packet_type].put_nowait(COMMAND_HANDLERS[packet_type](packet))
+        else:
+            print("Did not expect this packet")
 
     async with BleakClient(ADDRESS) as client:
         print("Connected")
-
-        device_info = client.services.get_service(DEVICE_INFO_UUID)
-        assert device_info
-
-        hw_info = device_info.get_characteristic(DEVICE_HW_UUID)
-        assert hw_info
-        x = await client.read_gatt_char(hw_info)
-        print(x)
-
-        fw_info = device_info.get_characteristic(DEVICE_FW_UUID)
-        assert fw_info
-        x = await client.read_gatt_char(fw_info)
-        print(x)
-
-        await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
+        print("Device info: ", await get_device_info(client))
 
         nus = client.services.get_service(UART_SERVICE_UUID)
         assert nus
         rx_char = nus.get_characteristic(UART_RX_CHAR_UUID)
         assert rx_char
 
-        print("Trying to get battery")
-        await client.write_gatt_char(rx_char, BATTERY_PACKET, response=False)
-        await asyncio.sleep(5)
+        await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
 
+        print("battery:", await get_battery(client, rx_char, queues))
 
-        print("Getting heart rate")
-        await get_heart_rate(client, rx_char)
+        await get_heart_rate(client, rx_char, queues)
 
-        print("Getting SpO2")
-        await get_spo2(client, rx_char)
-
+        await client.write_gatt_char(rx_char, GET_TODAY_STEPS_PACKET, response=False)
+        await asyncio.sleep(2)
 
 
 
