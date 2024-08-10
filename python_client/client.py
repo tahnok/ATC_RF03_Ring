cffdaf0af5913f4a5ef9decad5242ea0ac46a888
Wesley Ellis <tahnok@gmail.com>
2024-08-10 16:17:41 -0300

    Add set time cmd

--- python_client/client.py
+++ python_client/client.py
@@ -10,6 +10,7 @@ TODO:
 """
 
 from dataclasses import dataclass
+from datetime import datetime
 
 import asyncio
 from bleak import BleakClient
@@ -32,6 +33,7 @@ CMD_STOP_HEART_RATE = 106 # 0x6A
 CMD_BATTERY = 3
 CMD_BLINK_TWICE = 16 # 0x10
 CMD_GET_STEP_SOMEDAY = 67 #0x43
+CMD_SET_TIME = 1
 
 def make_packet(command_key: int, sub_data: bytearray|None = None) -> bytearray:
     packet = bytearray(16)
@@ -62,11 +64,33 @@ BLINK_TWICE_PACKET = make_packet(CMD_BLINK_TWICE)
 
 GET_TODAY_STEPS_PACKET = make_packet(CMD_GET_STEP_SOMEDAY, bytearray(b'\x00\x0F\x00\x5F\x01'))
 
-async def send_packet(client: BleakClient, rx_char, packet: bytearray) -> None:
-    await client.write_gatt_char(rx_char, packet, response=False)
+def byte_to_bcd(b: int) -> int:
+    assert b < 99
+    assert b > 0
+
+    tens = b // 10
+    ones = b % 10
+    return (tens << 4) | ones
 
 def bcd_to_decimal(b: int) -> int:
     return (((b >> 4) & 15) * 10) + (b & 15)
+
+def set_time_packet(target: datetime | None = None) -> bytearray:
+    if target is None:
+        target = datetime.now()
+    data = bytearray(7)
+    data[0] = byte_to_bcd(target.year % 2000)
+    data[1] = byte_to_bcd(target.month)
+    data[2] = byte_to_bcd(target.day)
+    data[3] = byte_to_bcd(target.hour)
+    data[4] = byte_to_bcd(target.minute)
+    data[5] = byte_to_bcd(target.second)
+    data[6] = 1 # set language to english, 0 is chinese
+    return make_packet(CMD_SET_TIME, data)
+
+async def send_packet(client: BleakClient, rx_char, packet: bytearray) -> None:
+    await client.write_gatt_char(rx_char, packet, response=False)
+
 
 @dataclass
 class SportDetail():
@@ -225,6 +249,8 @@ async def get_device_info(client: BleakClient):
 
     return data
 
+async def set_time(client: BleakClient, rx_char):
+    await send_packet(client, rx_char, set_time_packet())
 
 async def main():
     print("Connecting...")
@@ -238,7 +264,7 @@ async def main():
         if packet_type in COMMAND_HANDLERS:
             queues[packet_type].put_nowait(COMMAND_HANDLERS[packet_type](packet))
         else:
-            print("Did not expect this packet")
+            print("Did not expect this packet", packet)
 
     async with BleakClient(ADDRESS) as client:
         print("Connected")
@@ -254,6 +280,7 @@ async def main():
         print("battery:", await get_battery(client, rx_char, queues))
 
         #await get_heart_rate(client, rx_char, queues)
+        await set_time(client, rx_char)
 
         await client.write_gatt_char(rx_char, GET_TODAY_STEPS_PACKET, response=False)
         await asyncio.sleep(2)
