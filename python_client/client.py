84d9bf841d0a083678d9feae5f4d911ffac608e7
Wesley Ellis <tahnok@gmail.com>
2024-08-13 13:44:13 -0300

    sorta heart rate detail parsing

--- python_client/client.py
+++ python_client/client.py
@@ -3,14 +3,14 @@ A python client for connecting to the Colmi R02 Smart ring
 
 
 TODO:
-    - fetch "steps"
     - get "Stress"
     - make nicer methods for getting data from callback
     - "scan" mode instead of hard coded address
 """
 
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timezone
+import struct
 
 import asyncio
 from bleak import BleakClient
@@ -34,6 +34,7 @@ CMD_BATTERY = 3
 CMD_BLINK_TWICE = 16 # 0x10
 CMD_GET_STEP_SOMEDAY = 67 #0x43
 CMD_SET_TIME = 1
+CMD_READ_HEART_RATE = 21 # 0x15
 
 def make_packet(command_key: int, sub_data: bytearray|None = None) -> bytearray:
     packet = bytearray(16)
@@ -87,6 +88,13 @@ def set_time_packet(target: datetime | None = None) -> bytearray:
     data[5] = byte_to_bcd(target.second)
     data[6] = 1 # set language to english, 0 is chinese
     return make_packet(CMD_SET_TIME, data)
+
+def read_heart_rate_packet(target: datetime | None = None) -> bytearray:
+    if target is None:
+        target = datetime.now(timezone.utc).replace(hour=0,minute=0,second=0)
+    data = bytearray(struct.pack("<L", int(target.timestamp())))
+
+    return make_packet(CMD_READ_HEART_RATE, data)
 
 async def send_packet(client: BleakClient, rx_char, packet: bytearray) -> None:
     await client.write_gatt_char(rx_char, packet, response=False)
@@ -162,6 +170,73 @@ class SportDetailParser():
             self.index += 1
             if packet[5] == packet[6] - 1:
                 self.reset()
+
+class DailyHeartRateParser():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.heart_rate_array = []
+        self.m_utc_time = None
+        self.size = 0
+        self.index = 0
+        self.end = False
+        self.range = 5
+
+    def is_today(self) -> bool:
+        d = self.m_utc_time
+        if d is None:
+            return False
+        now = datetime.now() # use local time
+        return d.year == now.year and d.month == now.month and d.day == now.day
+
+    def parse(self, packet: bytearray) -> None:
+        r"""
+        first byte of packet should always be CMD_READ_HEART_RATE (21)
+        second byte is the sub_type
+
+        sub_type 0 contains the lengths of things
+        byte 2 is the number of expected packets after this.
+
+        example: bytearray(b'\x15\x00\x18\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x002'),
+
+
+        """
+
+        sub_type = packet[1]
+        if sub_type == 255 or (self.is_today() and sub_type == 23):
+            # reset?
+            return
+        if sub_type == 0:
+            self.end = False
+            self.size = packet[2] #number of expected readings or packets?
+            self.range = packet[3]
+            self.heart_rate_array = [-1] * (self.size * 13) # don't really need this but...
+        elif sub_type == 1:
+            # next 4 bytes are a timestamp
+            ts = struct.unpack_from("<l", packet, offset=2)[0]
+            self.m_utc_time = datetime.fromtimestamp(ts, timezone.utc)
+            #TODO timezone?
+
+            # remaining 16 - type - subtype - 4 - crc = 9
+            self.heart_rate_array[0:9] = list(packet[6:-1]) # I think this is the rest?
+            self.index += 9
+        else:
+            b = len(self.heart_rate_array)
+            print("packet", list(packet[2:15]))
+            print("slice", self.heart_rate_array[self.index:self.index+13])
+            print([x for x in self.heart_rate_array[self.index:self.index+13] if x != -1])
+            assert not [x for x in self.heart_rate_array[self.index:self.index+13] if x != -1]
+            self.heart_rate_array[self.index:self.index+13] = list(packet[2:15])
+            assert b == len(self.heart_rate_array)
+            self.index += 13
+            if sub_type == self.size - 1:
+                self.end = True
+                # probaby do a reset
+        self.end = True
+        # possibly do a reset
+        print("post self", self)
+
 
 def parse_heart_rate(packet: bytearray) -> dict[str, int]:
     return {
@@ -280,9 +355,11 @@ async def main():
         print("battery:", await get_battery(client, rx_char, queues))
 
         #await get_heart_rate(client, rx_char, queues)
-        await set_time(client, rx_char)
+        #await set_time(client, rx_char)
+        target = datetime(2024,8,10,0,0,0,0,tzinfo=timezone.utc)
+        await send_packet(client, rx_char, read_heart_rate_packet(target))
 
-        await client.write_gatt_char(rx_char, GET_TODAY_STEPS_PACKET, response=False)
+        #await client.write_gatt_char(rx_char, GET_TODAY_STEPS_PACKET, response=False)
         await asyncio.sleep(2)
 
 
